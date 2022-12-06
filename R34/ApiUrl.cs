170707fcbca90813a04344f7995570ea5db79de0
Katy248 <petrovanton247@gmail.com>
2022-12-06 12:28:35 +0300

    Reorganize urls

--- R34/ApiUrl.cs
+++ R34/ApiUrl.cs
@@ -8,10 +8,11 @@ public static class ApiUrl
     public static string Comments = $"{APIUrl}index.php?page=dapi&s=comment&q=index&post_id={{POST_ID}}";
     public static string UserFavorites = $"{APIUrl}index.php?page=favorites&s=view&id={{USR_ID}}";
     public static string GetPost = $"{APIUrl}index.php?page=dapi&s=post&q=index&id={{POST_ID}}";
+    public static string Pool = $"{APIUrl}index.php?page=pool&s=show&id={{POOL_ID}}";
+    public static string UserPage = $"{APIUrl}index.php?page=account&s=profile&id={{USER_ID}}";
+
     public static string ICame = $"{BaseUrl}index.php?page=icame";
     public static string RandomPost = $"{BaseUrl}index.php?page=post&s=random";
-    public static string UserPage = $"{APIUrl}index.php?page=account&s=profile&id={{USER_ID}}";
-    public static string Pool = $"{APIUrl}index.php?page=pool&s=show&id={{POOL_ID}}";
     public static string TopMap = $"{BaseUrl}index.php?page=toptags";
     public static string Stats = $"{BaseUrl}index.php?page=stats";
 }
