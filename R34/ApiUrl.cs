62a4904e55b47ea27c801d12f0db7bd204d33b10
Katy248 <petrovanton247@gmail.com>
2022-12-07 08:48:32 +0300

    Add tag url

--- R34/ApiUrl.cs
+++ R34/ApiUrl.cs
@@ -4,12 +4,14 @@ public static class ApiUrl
 {
     public const string APIUrl = "https://api.rule34.xxx/";
     public const string BaseUrl = "https://rule34.xxx/";
+
     public static string Search = $"{APIUrl}index.php?page=dapi&s=post&q=index&limit={{LIMIT}}&tags={{TAGS}}";
     public static string Comments = $"{APIUrl}index.php?page=dapi&s=comment&q=index&post_id={{POST_ID}}";
     public static string UserFavorites = $"{APIUrl}index.php?page=favorites&s=view&id={{USR_ID}}";
     public static string GetPost = $"{APIUrl}index.php?page=dapi&s=post&q=index&id={{POST_ID}}";
     public static string Pool = $"{APIUrl}index.php?page=pool&s=show&id={{POOL_ID}}";
     public static string UserPage = $"{APIUrl}index.php?page=account&s=profile&id={{USER_ID}}";
+    public static string Tags = $"{APIUrl}index.php?page=dapi&s=tag&q=index";
 
     public static string ICame = $"{BaseUrl}index.php?page=icame";
     public static string RandomPost = $"{BaseUrl}index.php?page=post&s=random";
