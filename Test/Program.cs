04e1800436d3692293df09f83f67426324973dff
Katy248 <petrovanton247@gmail.com>
2022-12-07 08:49:27 +0300

    Refactor test

--- Test/Program.cs
+++ Test/Program.cs
@@ -2,21 +2,16 @@
 
 var r34 = new Rule34();
 
-var posts = r34.Search(new string[] { "anal"}, 1, 12);
+var posts = r34.Search(new string[] { "boobs"}, 1, 500);
 foreach (var post in posts)
-{
-    Console.WriteLine($"- Post {post.Id} -");
-    Console.WriteLine($"Post owner: {post.Owner}; Image url: {post.Image}; Creation date: {post.created_at};");
-    Console.WriteLine($"Tags: {post.tags}");
-    Console.WriteLine("--");
-}
+    Console.WriteLine($" Post ID: {post.Id}; Post owner: {post.Owner}; Image url: {post.Image}; Creation date: {post.created_at};");
+
 var comments = r34.GetComments(7086496);
 foreach (var comment in comments)
-{
-    Console.WriteLine($"- Comment to post {comment.PostId} -");
-    Console.WriteLine($"ID: {comment.Id}; Creation date: {comment.Creation}; Body");
-    Console.WriteLine($"\t{comment.Body}");
-    Console.WriteLine("--");
-}
-//var posts2 = r34.GetPool(23299);
-Console.Error.WriteLine(r34.Random(new string[] { "-anal", "-futanari", "-gay" }, 10)?.Image);
+    Console.WriteLine($"Comment ID: {comment.Id}; Creation date: {comment.Creation}; Body:\n{comment.Body}");
+
+var tags = r34.GetTags(100);
+foreach (var tag in tags)
+    Console.WriteLine($"Tag ID: {tag.Id}; Name: {tag.Name}; Type:\n{tag.Type}");
+
+Console.WriteLine(r34.GetTag(22277).Name);
