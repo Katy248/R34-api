aefb65db2c5dc586c360727e07cb1984d9feee5b
Katy248 <petrovanton247@gmail.com>
2022-12-07 08:48:16 +0300

    Add tag class and its container

---
+++ R34/OrriginalApi/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace R34.OrriginalApi;
+
+[XmlType("tag")]
+public class Tag
+{
+    [XmlAttribute("id")]
+    public long Id { get; set; }
+    [XmlAttribute("name")]
+    public string Name { get; set; }
+    [XmlAttribute("type")]
+    public int Type { get; set; }
+    [XmlAttribute("count")]
+    public int Count { get; set; }
+    [XmlAttribute("ambiguous")]
+    public bool Ambiguous { get; set; }
+
+}
---
+++ R34/OrriginalApi/TagsContainer.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml.Serialization;
+
+namespace R34.OrriginalApi;
+
+[XmlType("tags")]
+public class TagsContainer
+{
+    [XmlElement("tag")]
+    public List<Tag> Tags { get; set; }
+}
